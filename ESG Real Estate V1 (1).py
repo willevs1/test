bcf061ec685e72bb25a8d5271bdde51ec6c215c7
willevs1 <wills.msn@hotmail.co.uk>
2025-07-11 14:44:24 +0100

    Update ESG Real Estate V1.py

--- ESG Real Estate V1 (1).py
+++ ESG Real Estate V1 (1).py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-!pip install matplotlib
+pip install matplotlib
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Building Decarbonization Model", layout="wide")
