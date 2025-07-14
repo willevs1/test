090a18b73db3b77cf861266103e29603b902ede8
willevs1 <wills.msn@hotmail.co.uk>
2025-07-14 09:45:22 +0100

    Update Route to Target Intensity.py

--- pages/test3.py
+++ pages/test3.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.title("Hello Streamlit!")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.set_page_config(
+    page_title="ESG Analysis",
+    page_icon="📊",
+    layout="wide"
+)
