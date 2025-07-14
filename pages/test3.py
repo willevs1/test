e1c289e85a005c690f5ff3387d37098564f68cb9
willevs1 <wills.msn@hotmail.co.uk>
2025-07-14 09:52:36 +0100

    Update Retrofits.py

--- pages/test3.py
+++ pages/test3.py
@@ -8,3 +8,17 @@ st.set_page_config(
     page_icon="📊",
     layout="wide"
 )
+
+
+# Retrofit measures
+st.sidebar.header("Retrofit Measures (per m²)")
+retrofits = pd.DataFrame({
+    "Measure": ["BMS Replacement", "Reduced Tennant Load", "Low Energy Light", "Night Purging"],
+    "Capex €/m²": [40, 142, 142, 142],
+    "kWh saved/m²/year": [130, 121.6, 121.6, 121.6]
+}).set_index("Measure")
+
+selected_measures = st.sidebar.multiselect(
+    "Select Retrofit Measures:",
+    options=retrofits.index
+)
